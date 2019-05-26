16a29a1b66e4201f3a60cfa7ddee97b25ff3cbd2
Jed Cyril <trinidadjedcyril@gmail.com>
2019-05-26 15:40:35 +0800

    Refactor

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,7 @@
   <div class="about">
     <h1>This is an about page for knows-mo-to</h1>
     <p>
-      Questions from <a href="https://opentdb.com/">Open Trivia Database</a>
+      Questions from <a href="https://opentdb.com/" target="_blank">Open Trivia Database</a>
     </p>
   </div>
 </template>
