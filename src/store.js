a82418fc2d65ddc06d2d5f9b1e61652151c65ca4
Jed Cyril <trinidadjedcyril@gmail.com>
2019-05-27 21:25:19 +0800

    Add Category Functionality

--- src/store.js
+++ src/store.js
@@ -6,11 +6,17 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     categories: [],
+    questions: [],
+    question: Object,
     isLoading: false
   },
   mutations: {
     setCategories: (state, categories) => {
       state.categories = categories
+    },
+    setQuestions: (state, questions) => {
+      state.question = questions.shift()
+      state.questions = questions
     }
   },
   actions: {
@@ -18,7 +24,25 @@ export default new Vuex.Store({
       try {
         let response = await fetch("https://opentdb.com/api_category.php")
         let data = await response.json()
-        context.commit('setCategories', data.trivia_categories)
+        if(context.state.categories.length === 0) {
+          context.commit('setCategories', data.trivia_categories)
+        }
+      } catch (error) {
+        alert(error)
+      }
+    },
+    async getQuestions(context, categoryId) {
+      try {
+        if(categoryId > 0 && categoryId != null) {
+          let response = await fetch(`https://opentdb.com/api.php?amount=10&type=multiple&category=${categoryId}`)
+          let data = await response.json()
+          context.commit('setQuestions', data.results)
+        }
+        else {
+          let response = await fetch("https://opentdb.com/api.php?amount=10&type=multiple")
+          let data = await response.json()
+          context.commit('setQuestions', data.results)
+        }
       } catch (error) {
         alert(error)
       }
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,13 +1,34 @@
 <template>
-    <div class="game">
-        
+    <div id="game">
+        <loading :active.sync="isLoading" :can-cancel="false" loader="dots"></loading>
     </div>
 </template>
 
 <script>
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/vue-loading.css'
 
 export default {
-    name: 'game'
+    name: 'game',
+    components: {
+        Loading
+    },
+    data() {
+        return {
+            isLoading: false,
+            question: Object,
+            questions: []
+        }
+    },
+    created() {
+        this.isLoading = true
+        this.$store.dispatch('getQuestions', this.$route.id)
+            .then(() => {
+                this.isLoading = false
+                this.question = this.$store.state.question
+                this.questions = this.$store.state.questions
+            })
+    }
 }
 </script>
 
